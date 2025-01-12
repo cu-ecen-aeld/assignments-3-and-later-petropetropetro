4e198fbbf19df7ff46b6d8ceaa7a1bd51796aea7
petropetropetro <maleshpetro7@gmail.com>
2025-01-12 21:45:59 +0200

    Threading

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -14,6 +14,23 @@ void* threadfunc(void* thread_param)
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* data = (struct thread_data*) thread_param;
+
+    usleep(data->wait_to_obtain_ms * 1000);
+
+    if (pthread_mutex_lock(data->mutex) != 0) {
+        data->thread_complete_success = false;
+        return thread_param;
+    }
+
+    usleep(data->wait_to_release_ms * 1000);
+
+    if (pthread_mutex_unlock(data->mutex) != 0) {
+        data->thread_complete_success = false;
+        return thread_param;
+    }
+
+    data->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +45,25 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+    struct thread_data *data = (struct thread_data*) malloc(sizeof(struct thread_data));
+    if (!data) {
+        perror("Failed to allocate memory for thread_data");
+        return false;
+    }
+
+    data->mutex = mutex;
+    data->wait_to_obtain_ms = wait_to_obtain_ms;
+    data->wait_to_release_ms = wait_to_release_ms;
+    data->thread_complete_success = false;
+
+    if (pthread_create(thread, NULL, threadfunc, data) != 0) {
+        perror("Failed to create thread");
+        free(data);
+        return false;
+    }
+
+    return true;
+
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -7,6 +7,7 @@
  * It should be returned by your thread so it can be freed by
  * the joiner thread.
  */
+
 struct thread_data{
     /*
      * TODO: add other values your thread will need to manage
@@ -19,6 +20,9 @@ struct thread_data{
      * Set to true if the thread completed with success, false
      * if an error occurred.
      */
+    pthread_mutex_t *mutex;
+    int wait_to_obtain_ms;
+    int wait_to_release_ms;
     bool thread_complete_success;
 };
 
